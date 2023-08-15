3c05067506d8b98fcec255cc10ef38c1df30161f
david <david@divlogic.net>
2023-08-14 23:12:00 -0600

    add multiple asteroids

--- vite/src/main.ts
+++ vite/src/main.ts
@@ -1,13 +1,7 @@
-import kontra from 'kontra'
+import { Sprite, init, GameLoop } from 'kontra'
 
 const {
-  init,
-  Sprite,
-  GameLoop
-} = kontra
-
-const {
-  canvas, context
+  canvas
 } = init()
 
 const sprite = Sprite({
@@ -19,57 +13,67 @@ const sprite = Sprite({
   dx: 2
 
 })
+const asteroids: Sprite[] = []
 
-const asteroid = Sprite({
-  type: 'asteroid',
-  x: 100,
-  y: 100,
-  dx: Math.random() * 4 - 2,
-  dy: Math.random() * 4 - 2,
-  radius: 30,
-  color: 'orange',
-  render () {
-    if (this.context != null) {
-      this.context.strokeStyle = 'white'
-      this.context.beginPath()
-      this.context.arc(0, 0, this.radius, 0, Math.PI * 2)
-      this.context.stroke()
+function createAsteroid (): void {
+  const asteroid = Sprite({
+    type: 'asteroid',
+    x: 100,
+    y: 100,
+    dx: Math.random() * 4 - 2,
+    dy: Math.random() * 4 - 2,
+    radius: 30,
+    render () {
+      if (this.context != null) {
+        this.context.strokeStyle = 'white'
+        this.context.beginPath()
+        this.context.arc(0, 0, this.radius, 0, Math.PI * 2)
+        this.context.stroke()
+      }
     }
-  }
-})
+  })
+  asteroids.push(asteroid)
+}
+
+for (let i = 0; i < 4; i++) {
+  createAsteroid()
+}
+console.log(asteroids)
 
 const loop = GameLoop({
   update: function () {
     sprite.update()
-    asteroid.update()
 
     if (sprite.x > canvas.width - 300) {
       sprite.x = -sprite.width
     }
-
+    asteroids.forEach(asteroid => {
     // asteroid is beyond the left edge
-    if (asteroid.x < -asteroid.radius) {
-      asteroid.x = canvas.width + asteroid.radius
-      console.log('asteroid action 1')
-    }
-    // asteroid is beyond the right edge
-    else if (asteroid.x > canvas.width + asteroid.radius) {
-      asteroid.x = 0 - asteroid.radius
-      console.log('asteroid action 2')
-    }
-    // asteroid is beyond the top edge
-    if (asteroid.y < -asteroid.radius) {
-      asteroid.y = canvas.height + asteroid.radius
-      console.log('asteroid action 3')
-    }
-    // asteroid is beyond the bottom edge
-    else if (asteroid.y > canvas.height + asteroid.radius) {
-      asteroid.y = -asteroid.radius
-      console.log('asteroid action 4')
-    }
+      const radius: number = asteroid.radius
+      if (asteroid.x < -radius) {
+        asteroid.x = canvas.width + radius
+        console.log('asteroid action 1')
+      } else if (asteroid.x > canvas.width + radius) {
+      // asteroid is beyond the right edge
+        asteroid.x = 0 - radius
+        console.log('asteroid action 2')
+      }
+      // asteroid is beyond the top edge
+      if (asteroid.y < -radius) {
+        asteroid.y = canvas.height + radius
+        console.log('asteroid action 3')
+      } else if (asteroid.y > canvas.height + radius) {
+      // asteroid is beyond the bottom edge
+        asteroid.y = -radius
+        console.log('asteroid action 4')
+      }
+      asteroid.update()
+    })
   },
   render: function () {
-    asteroid.render()
+    asteroids.forEach(asteroid => {
+      asteroid.render()
+    })
     sprite.render()
   }
 })
