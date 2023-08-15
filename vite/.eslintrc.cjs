6b5ca5b03254a63c77e348d05c8da00016fe37fe
david <david@divlogic.net>
2023-08-15 01:18:47 -0600

    fix linting and formatting

--- vite/.eslintrc.cjs
+++ vite/.eslintrc.cjs
@@ -1,60 +1,40 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2021': true
-    },
-    'extends': [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'standard-with-typescript'
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'standard-with-typescript',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    '@typescript-eslint/explicit-function-return-type': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      {
+        checksVoidReturn: false,
+      },
     ],
-    'overrides': [
-        {
-            'files': ['*.js', '*.jsx', '*.ts', '*.tsx'],
-            'extends': 'standard-with-typescript',
-            'parserOptions': {
-              'project': './tsconfig.json'
-            }
-          }
-    ],
-    'parser': '@typescript-eslint/parser',
-    'parserOptions': {
-        'ecmaVersion': 'latest',
-        'sourceType': 'module',
-        'project': './tsconfig.json',
-        'tsconfigRootDir': __dirname
-    },
-    'plugins': [
-        '@typescript-eslint'
-    ],
-    'rules': {
-        'indent': [
-            'error',
-            4
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'always'
-        ],
-        '@typescript-eslint/strict-boolean-expressions': 'warn',
-        '@typescript-eslint/explicit-function-return-type': 'warn',
-        '@typescript-eslint/no-unused-vars': 'warn',
-        "@typescript-eslint/no-misused-promises": [
-          "error",
-          {
-            "checksVoidReturn": false
-          }
-        ],
-        'object-property-newline': 'warn',
-        'object-curly-newline': ['warn', 'always'],
-        'max-len': ['warn'],
-    }
+    'object-property-newline': 'warn',
+    // This one might not mesh with prettier
+    // 'object-curly-newline': ['warn', 'always'],
+    'max-len': ['warn'],
+    'comma-dangle': ['off'],
+    '@typescript-eslint/comma-dangle': 'off',
+    semi: 'off',
+    '@typescript-eslint/semi': ['error', 'always'],
+  },
 };
